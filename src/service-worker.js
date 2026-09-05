186cae5b4d994f714cf9e7c43e594d0429b4b151
Mike Macfadden <mikemacfadden@gmail.com>
2026-09-05 12:31:22 -0500

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   'js/events.js',
   'js/storage.js',
   'js/calculations.js',
-  'data/foods.js?v=1.8',
+  'data/foods.js?v=1.9',
   'images/favicon.png',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.js'
